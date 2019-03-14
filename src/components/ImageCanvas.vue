9241a3c86c4d4b75f3b78d3c8101fa0e4e324a5f
bartosz.gamza <bartosz.gamza12@gmail.com>
2019-03-14 18:32:24 +0100

    added color blocks rendering

--- src/components/ImageCanvas.vue
+++ src/components/ImageCanvas.vue
@@ -3,6 +3,15 @@
     <img ref="sampleImage" :src="sampleImageSource" alt="sample">
     <button @click="renderCanvas">Render</button>
     <canvas ref="imageCanvas"></canvas>
+    <div v-if="topTenColors.length">
+      <div
+        v-for="(color, i) in topTenColors"
+        :key="i"
+        class="colorBlock"
+        :style="boxColor(color)"
+      >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,16 +19,28 @@
 export default {
   data () {
     return {
-      pixelsColorArray: []
+      topTenColors: []
     }
   },
   methods: {
+    boxColor (color) {
+      return {
+        backgroundColor: `hsl(${color})`
+      }
+    },
     chunkArray (arr, size, offset) {
       let results = []
       for (let i = 0; i < arr.length; i += size) {
         results.push(arr.slice(i, i + (size - offset)))
       }
       return results
+    },
+    decodeColor ([h, s, l]) {
+      return `${h}, ${s}%, ${l}%`
+    },
+    hslArray (code) {
+      let codeArr = code.split('.').map(x => parseInt(x))
+      return [codeArr[0] * 16, codeArr[1] * 10, codeArr[2] * 10]
     },
     renderCanvas () {
       let canvas = this.$refs.imageCanvas
@@ -31,7 +52,7 @@ export default {
 
       context.drawImage(image, 0, 0)
       const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
-      console.log(imageData.data)
+      // console.log(imageData.data)
 
       // 161, 170, 201, 255
       // notes:
@@ -40,13 +61,29 @@ export default {
       const chunkedArray = this.chunkArray(imageData.data, 4, 1)
       const firstPixel = chunkedArray[0]
       const firstPixelHsv = this.rgbToHsv(firstPixel)
-      console.log(firstPixel, firstPixelHsv)
+      // console.log(firstPixel, firstPixelHsv)
 
       const normalizedHsvArray = chunkedArray.map(arr => this.rgbToHsv(arr))
-      console.log(normalizedHsvArray)
-      console.log('most: ', this.mode(normalizedHsvArray))
+      // console.log(normalizedHsvArray)
+      const topTen = this.mostOccurencies(normalizedHsvArray)
+
+      const colors = topTen.map(c => this.hslArray(c)).map(a => this.decodeColor(a))
+      this.topTenColors = colors
 
       // https://stackoverflow.com/questions/1106190/algorithm-challenge-generate-color-scheme-from-an-image
+    },
+    mostOccurencies (arr) {
+      let colorMap = {}
+
+      for (let i = 0; i < arr.length; i++) {
+        let currentElement = arr[i].join('.')
+        if (!colorMap[currentElement]) {
+          colorMap[currentElement] = 1
+        } else {
+          colorMap[currentElement]++
+        }
+      }
+      return Object.keys(colorMap).sort((a,b) => {return colorMap[b]-colorMap[a]}).slice(0, 10)
     },
     rgbToHsv ([r, g, b]) {
       // http://www.javascripter.net/faq/rgb2hsv.htms
@@ -63,7 +100,7 @@ export default {
       // Black-gray-white
       if (minRGB === maxRGB) {
         computedV = minRGB
-        return [0, 0, computedV]
+        return [0, 0, computedV] // TODO normalize !
       }
 
       // Colors other than black-gray-white:
@@ -73,6 +110,9 @@ export default {
       computedS = (maxRGB - minRGB) / maxRGB
       computedV = maxRGB
       return this.normalizeHsv([Math.round(computedH), Math.round(computedS * 100), Math.round(computedV * 100)])
+    },
+    sortTopTen (obj) {
+      return Object.keys(obj).sort((a,b) => {return obj[b]-obj[a]}).slice(0, 10)
     },
     normalizeHsv ([h, s, v]) {
       // reduce number of *slightly* different colors
@@ -86,3 +126,11 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .colorBlock {
+    width: 100px;
+    height: 100px;
+  }
+</style>
+
